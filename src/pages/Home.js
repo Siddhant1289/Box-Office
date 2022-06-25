ebde4d122503e79afb125ec7906e025183fb7187
Siddhant Verma <karanspd1289@gmail.com>
2022-06-26 00:39:30 +0530

    added layout management and created title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <div>This Is Home</div>;
+  return <MainPageLayout>This Is Home</MainPageLayout>;
 };
 
 export default Home;
--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Starred = () => {
-  return <div>This Is Starred</div>;
+  return <MainPageLayout>This Is Starred</MainPageLayout>;
 };
 
 export default Starred;
