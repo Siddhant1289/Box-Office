01d319dbfa1a60eeb903350fa06dbb50c46399bf
Siddhant Verma <karanspd1289@gmail.com>
2022-06-29 11:32:17 +0530

    created two ew hooks: useLastQuerry and useShow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import Actorgrid from '../components/actor/Actorgrid';
 import MainPageLayout from '../components/MainPageLayout';
-import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+//import ActorGrid from '../components/actor/ActorGrid';
+import { useLastQuery } from '../misc/custom-hooks';
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
 
   const isShowsSearch = searchOption === 'shows';
-
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
@@ -40,7 +40,7 @@ const Home = () => {
       return results[0].show ? (
         <ShowGrid data={results} />
       ) : (
-        <Actorgrid data={results} />
+        <ActorGrid data={results} />
       );
     }
 
