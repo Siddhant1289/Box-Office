92d4ac1b738beda2135c59d3ebf95bfa16a4746d
Siddhant Verma <karanspd1289@gmail.com>
2022-06-29 13:11:47 +0530

    Updates

---
+++ precache-manifest.dddae7c78b88eac0f01bbaba38c90ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d8be164be94365a0ca527ad77e3e60",
+    "url": "/Box-Office/index.html"
+  },
+  {
+    "revision": "be0cb63a9d89231b0c37",
+    "url": "/Box-Office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "aae627a0352963282016",
+    "url": "/Box-Office/static/js/2.cb250a61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-Office/static/js/2.cb250a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0cb63a9d89231b0c37",
+    "url": "/Box-Office/static/js/main.3e46e80f.chunk.js"
+  },
+  {
+    "revision": "6740a4f854f98d4fa166",
+    "url": "/Box-Office/static/js/runtime-main.140a3920.js"
+  }
+]);
