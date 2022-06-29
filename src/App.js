0f216eda070c0e3c07f8a85bf0848b3954f3e695
Siddhant Verma <karanspd1289@gmail.com>
2022-06-29 10:10:30 +0530

    created show page components and populated them with data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disabled */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
--- src/components/show/ShowMainData.js
+++ src/components/show/ShowMainData.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import IMG_PLACEHOLDER from '../../images/not-found.png';
+import { Star } from '../styled';
 
-const showMainData = () => {
-  return <div>x</div>;
+const ShowMainData = ({ name, rating, summary, tags, image }) => {
+  return (
+    <div>
+      <img src={image ? image.original : IMG_PLACEHOLDER} alt="show-cover" />
+      <div>
+        <div>
+          <h1>{name}</h1>
+          <div>
+            <Star />
+            <span>{rating.average || 'N/A'}</span>
+          </div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: summary }} />
+
+        <div>
+          Tags:{' '}
+          <div>
+            {tags.map((tag, i) => (
+              <span key={i}>{tag}</span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default showMainData;
+export default ShowMainData;
